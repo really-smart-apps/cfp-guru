c71305ba90ab3e6815f272cb3d42a5e192ffd6be
Anuj Singla <asingla@redhat.com>
2022-08-07 01:16:26 +0530

    very basic project setup .

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,32 @@
+import {
+  Masthead,
+  MastheadBrand,
+  MastheadContent,
+  MastheadMain,
+  Page,
+  PageSection,
+  PageSectionVariants,
+} from "@patternfly/react-core";
+
+interface IAppLayout {
+  children: React.ReactNode;
+}
+export const mainContentId = "main-content-page-layout";
+
+export function AppLayout({ children }: IAppLayout) {
+  const header = (
+    <Masthead id="basic">
+      <MastheadMain>
+        <MastheadBrand>Logo</MastheadBrand>
+      </MastheadMain>
+      <MastheadContent>
+        <span>Content</span>
+      </MastheadContent>
+    </Masthead>
+  );
+  return (
+    <Page sidebar={null} mainContainerId={mainContentId} header={header}>
+      <PageSection variant={PageSectionVariants.light}>{children}</PageSection>
+    </Page>
+  );
+}
