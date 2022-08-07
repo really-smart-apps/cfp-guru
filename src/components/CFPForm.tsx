e207d8c4c11139c5d2bed2cf80e3e1880d932985
Anuj Singla <asingla@redhat.com>
2022-08-07 14:43:43 +0530

    Routes and home page setup .

--- src/components/CFPForm.tsx
+++ src/components/CFPForm.tsx
@@ -14,27 +14,9 @@ export function CFPForm() {
       <CardTitle>Header</CardTitle>
       <CardBody>
         <Form>
-          <FormGroup label="Cluster" fieldId="cluster-01">
-            {/* <ClusterDropdown
-              ariaLabel="Cluster env"
-              id="cluster-01"
-              name="cluster-01"
-            /> */}
-          </FormGroup>
-          <FormGroup label="Project" fieldId="project-01">
-            {/* <ProjectDropdown
-              ariaLabel="Project name"
-              id="project-01"
-              name="project-01"
-            /> */}
-          </FormGroup>
-          <FormGroup label="Console" fieldId="console-01">
-            {/* <ConsoleDropdown
-              ariaLabel="Console name"
-              id="console-01"
-              name="console-01"
-            /> */}
-          </FormGroup>
+          <FormGroup label="Cluster" fieldId="cluster-01"></FormGroup>
+          <FormGroup label="Project" fieldId="project-01"></FormGroup>
+          <FormGroup label="Console" fieldId="console-01"></FormGroup>
 
           <ActionGroup>
             <Button variant="primary">Submit</Button>
