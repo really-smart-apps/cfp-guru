9a7d8e59681e62b7a448d629bebd6d498dc24143
kouldeep@gmail.com <dkoul@redhat.com>
2022-08-29 15:37:07 +0530

    Added 'more help' hyperlink to adplist mentor page

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,5 +1,6 @@
 import {
   Bullseye,
+  Button,
   Card,
   CardBody,
   CardFooter,
@@ -18,6 +19,8 @@ export function AboutUs() {
           </div>
         </TextContent>
       </CardBody>
+      <CardBody><div className="pf-u-text-align-center"><TextContent>Need more help? Set up a free mentorship session with an expert</TextContent><br /><br /><Button component="a" href="https://adplist.org/mentors/deepak-koul-hehimhis" target="_blank" key="Need More Help?" variant="primary">
+Find Slots</Button></div></CardBody><br /><br /><br /><br />
       <CardFooter className="pf-u-text-align-center">
         Made with ❤ for aspiring speakers | Copyright © 2022 cfp-guru
       </CardFooter>
